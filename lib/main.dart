6e4fbef3e46e60ec33909b784aa572516716815d
mukeshsignal <mukesh.singal@outlook.com>
2019-05-21 08:11:28 +0530

    Added Alarm Service in Main.dart to print Hello world in log every minute

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:isolate';
+
 import 'package:daily_quotes/screens/CategoryPage.dart';
 import 'package:flutter/material.dart';
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 
-void main() {
+void printHello() {
+  final DateTime now = DateTime.now();
+  final int isolateId = Isolate.current.hashCode;
+  print("[$now] Hello, world! isolate=${isolateId} function='$printHello'");
+}
+
+void main() async {
+  final int helloAlarmID = 0;
+  await AndroidAlarmManager.initialize();
   runApp(MyApp());
+  await AndroidAlarmManager.periodic(
+      const Duration(minutes: 1), helloAlarmID, printHello);
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +23,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Daily Quotes',
-      
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
